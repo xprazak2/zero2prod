5243e44a823390d6264bc38e271c33ad491866c4
Ondrej Prazak <o.prazak5@seznam.cz>
2022-02-26 15:35:41 +0100

    Refactored structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    run(listener)?.await
 }
